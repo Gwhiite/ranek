2bf05ddb4f13367ac0cf4d86884903f58da130fa
gwhiite <guibrancodecampos@gmail.com>
2024-11-25 03:05:35 -0300

    Feat: Header & Footer boilerplate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-    <h1>Home page</h1>
-  </div>
+  <section>
+    <h1>Compre ou Venda 😎</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
   name: "HomeView",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
